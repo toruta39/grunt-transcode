feea148d1244e4f808c291159201c65fd70e88bd
Joshua Zhang <toruta39@gmail.com>
2014-07-11 00:57:09 +0900

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+/*
+ * grunt-transcode
+ * https://github.com/toruta39/grunt-transcode
+ *
+ * Copyright (c) 2014 Joshua Zhang
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    test_vars: {
+      name: 'grunt-contrib-copy',
+      version: '0.1.0',
+      match: 'folder_one/*'
+    },
+
+    // Configuration to be run (and then tested).
+    transcode: {
+      main: {
+        files: [
+          {expand: true, cwd: 'test/fixtures', src: ['*.*'], dest: 'tmp/transcode_test_files/'},
+          {expand: true, cwd: 'test/fixtures', src: ['**'], dest: 'tmp/transcode_test_mix/'},
+          {expand: true, cwd: 'test/fixtures', src: ['<%= test_vars.match %>'], dest: 'tmp/transcode_test_v<%= test_vars.version %>/'}
+        ]
+      },
+
+      flatten: {
+        files: [
+          {expand: true, flatten: true, filter: 'isFile', src: ['test/fixtures/**'], dest: 'tmp/transcode_test_flatten/'}
+        ]
+      },
+
+      single: {
+        files: [
+          {src: ['test/fixtures/test.js'], dest: 'tmp/single.js'}
+        ]
+      },
+
+      verbose: {
+        files: [
+          {expand: true, src: ['test/fixtures/**'], dest: 'tmp/transcode_test_verbose/'}
+        ]
+      },
+
+      mode: {
+        options: {
+          mode: '0444',
+        },
+        src: ['test/fixtures/test2.js'],
+        dest: 'tmp/mode.js',
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'transcode', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/transcode.js
@@ -0,0 +1,95 @@
+/*
+ * grunt-transcode
+ * https://github.com/toruta39/grunt-transcode
+ *
+ * Copyright (c) 2014 Joshua Zhang
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  var path = require('path');
+  var fs = require('fs');
+  var iconv = require('iconv-lite');
+
+  grunt.registerMultiTask('transcode', 'A file encoding converter that really works!', function () {
+
+    var options = this.options({
+      fromEncoding: 'utf8',
+      toEncoding: 'utf8',
+      processContentExclude: [],
+      mode: false
+    });
+
+    var copyOptions = {
+      encoding: 'binary',
+      process: function(content) {
+        content = iconv.decode(new Buffer(content, 'binary'), options.fromEncoding);
+        content = iconv.encode(content, options.toEncoding);
+
+        return content;
+      },
+      noProcess: options.noProcess || options.processContentExclude,
+    };
+
+    var dest;
+    var isExpandedPair;
+    var tally = {
+      dirs: 0,
+      files: 0
+    };
+
+    this.files.forEach(function(filePair) {
+      isExpandedPair = filePair.orig.expand || false;
+
+      filePair.src.forEach(function(src) {
+        if (detectDestType(filePair.dest) === 'directory') {
+          dest = (isExpandedPair) ? filePair.dest : unixifyPath(path.join(filePair.dest, src));
+        } else {
+          dest = filePair.dest;
+        }
+
+        if (grunt.file.isDir(src)) {
+          grunt.verbose.writeln('Creating ' + dest.cyan);
+          grunt.file.mkdir(dest);
+          tally.dirs++;
+        } else {
+          grunt.verbose.writeln('Converting ' + src.cyan + ' -> ' + dest.cyan);
+          grunt.file.copy(src, dest, copyOptions);
+          if (options.mode !== false) {
+            fs.chmodSync(dest, (options.mode === true) ? fs.lstatSync(src).mode : options.mode);
+          }
+          tally.files++;
+        }
+      });
+    });
+
+    if (tally.dirs) {
+      grunt.log.write('Created ' + tally.dirs.toString().cyan + ' directories');
+    }
+
+    if (tally.files) {
+      grunt.log.write((tally.dirs ? ', converted ' : 'Converted ') + tally.files.toString().cyan + ' files');
+    }
+
+    grunt.log.writeln();
+  });
+
+  var detectDestType = function(dest) {
+    if (grunt.util._.endsWith(dest, '/')) {
+      return 'directory';
+    } else {
+      return 'file';
+    }
+  };
+
+  var unixifyPath = function(filepath) {
+    if (process.platform === 'win32') {
+      return filepath.replace(/\\/g, '/');
+    } else {
+      return filepath;
+    }
+  };
+};
